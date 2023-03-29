b9cdd7cc89c7ae79e50e802dd81b7368e7bcf3a6
gra0298 <gra029801@gmail.com>
2023-03-29 08:15:40 -0500

    feat:JQuey and css add

--- css desde JQuery/jquery.js
+++ css desde JQuery/jquery.js
@@ -1,4 +1,25 @@
 
 $(document).ready(function(){
-    console.log("Documento listo");
+    // console.log("Documento listo");
+    $('h1').addClass('amarillo');//clase asiganada
+    $('p').removeClass('parrafo'); //eliminar clase
+    // intercambio añade o borra
+    $('button').click(function(){
+        $('p').toggleClass('parrafo'); //aparece desaparece tocando el botton
+        console.log($('p').css('font-size'));
+        console.log($('p').css('margin'));
+        $('div').innerWidth(240);
+        $('div').outerHeight(340);
+    });
+    // obtener valor de las propiedades del documento
+    console.log($('p').css('font-size'));
+    console.log($('p').css('margin'));
+    // asignar o cambiar valor
+    $('p').css({'background-color':'pink','font-size':'1.3em'});
+    // cambiar dimensiones caja div
+    $('div').width(120);
+    $('div').height(140);
+    // innerWith outerWidth innerHeight outerHeight
+
+
 });
